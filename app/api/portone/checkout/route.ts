86af74b55e01aa9bfd3df3f766d6df641a6e4f3d
YoungCheon Kim <kyt031522@gmail.com>
2025-03-03 21:56:46 +0900

    feat: checkout api에서 선제적으로 team db 업데이트

--- app/api/portone/checkout/route.ts
+++ app/api/portone/checkout/route.ts
@@ -9,6 +9,7 @@ import { eq } from 'drizzle-orm';
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const sessionId = searchParams.get('sessionId');
+  const isTrial = searchParams.get('isTrial') || false;
 
   if(!sessionId){
     return NextResponse.redirect(new URL('/pricing', request.url));
@@ -35,10 +36,20 @@ export async function GET(request: NextRequest) {
   const paymentId = _session[0].paymentId;
   const productId = _session[0].productId;
   const priceId = _session[0].priceId;
+  const product = await getProductById(productId);
 
   if(!paymentId || !productId || !priceId){
     return NextResponse.redirect(new URL('/pricing?error=no_verify_session', request.url));
   }
+
+  //team 정보 업데이트
+  await db.update(teams).set({
+    subscriptionStatus: isTrial === 'true' ? 'trial' : 'active',
+    subscriptionId: _session[0].id.toString(),
+    productId: productId,
+    planName: product.name,
+    shouldTrial: false,
+  }).where(eq(teams.id, team.id));
 
   return NextResponse.redirect(new URL(`/dashboard`, request.url));
 }
