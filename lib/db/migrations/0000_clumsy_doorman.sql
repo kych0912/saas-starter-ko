0549f7e544a211b2855c3aff6571f72a36236ac7
YoungCheon Kim <kyt031522@gmail.com>
2025-03-29 23:45:06 +0900

    chore:db migrate

--- lib/db/migrations/0000_clumsy_doorman.sql
+++ lib/db/migrations/0000_clumsy_doorman.sql
@@ -7,14 +7,6 @@ CREATE TABLE "activity_logs" (
 	"ip_address" varchar(45)
 );
 --> statement-breakpoint
-CREATE TABLE "billing_keys" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"team_id" integer,
-	"key" varchar(255) NOT NULL,
-	"created_at" timestamp DEFAULT now(),
-	"updated_at" timestamp DEFAULT now()
-);
---> statement-breakpoint
 CREATE TABLE "invitations" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"team_id" integer NOT NULL,
@@ -23,40 +15,6 @@ CREATE TABLE "invitations" (
 	"invited_by" integer NOT NULL,
 	"invited_at" timestamp DEFAULT now() NOT NULL,
 	"status" varchar(20) DEFAULT 'pending' NOT NULL
-);
---> statement-breakpoint
-CREATE TABLE "prices" (
-	"id" varchar PRIMARY KEY NOT NULL,
-	"product_id" varchar,
-	"currency" varchar NOT NULL,
-	"unit_amount" numeric NOT NULL,
-	"trial_period_days" integer,
-	"interval" integer NOT NULL,
-	"interval_count" integer,
-	"active" boolean DEFAULT true,
-	"created_at" timestamp DEFAULT now(),
-	"updated_at" timestamp DEFAULT now()
-);
---> statement-breakpoint
-CREATE TABLE "products" (
-	"id" varchar PRIMARY KEY NOT NULL,
-	"name" text NOT NULL,
-	"description" text,
-	"active" boolean DEFAULT true,
-	"created_at" timestamp DEFAULT now(),
-	"updated_at" timestamp DEFAULT now()
-);
---> statement-breakpoint
-CREATE TABLE "session" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"payment_id" varchar NOT NULL,
-	"billing_key" varchar(255) NOT NULL,
-	"team_id" integer NOT NULL,
-	"customer_id" varchar NOT NULL,
-	"product_id" varchar NOT NULL,
-	"price_id" varchar NOT NULL,
-	"created_at" timestamp DEFAULT now(),
-	"updated_at" timestamp DEFAULT now()
 );
 --> statement-breakpoint
 CREATE TABLE "team_members" (
@@ -72,14 +30,14 @@ CREATE TABLE "teams" (
 	"name" varchar(100) NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL,
-	"customer_id" text,
-	"subscription_id" text,
-	"product_id" text,
+	"steppay_customer_id" text,
+	"steppay_subscription_id" text,
+	"steppay_product_code" text,
+	"steppay_price_code" text,
 	"plan_name" varchar(50),
 	"subscription_status" varchar(20),
-	"should_trial" boolean DEFAULT true,
-	CONSTRAINT "teams_customer_id_unique" UNIQUE("customer_id"),
-	CONSTRAINT "teams_subscription_id_unique" UNIQUE("subscription_id")
+	CONSTRAINT "teams_steppay_customer_id_unique" UNIQUE("steppay_customer_id"),
+	CONSTRAINT "teams_steppay_subscription_id_unique" UNIQUE("steppay_subscription_id")
 );
 --> statement-breakpoint
 CREATE TABLE "users" (
@@ -96,12 +54,7 @@ CREATE TABLE "users" (
 --> statement-breakpoint
 ALTER TABLE "activity_logs" ADD CONSTRAINT "activity_logs_team_id_teams_id_fk" FOREIGN KEY ("team_id") REFERENCES "public"."teams"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "activity_logs" ADD CONSTRAINT "activity_logs_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "billing_keys" ADD CONSTRAINT "billing_keys_team_id_teams_id_fk" FOREIGN KEY ("team_id") REFERENCES "public"."teams"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "invitations" ADD CONSTRAINT "invitations_team_id_teams_id_fk" FOREIGN KEY ("team_id") REFERENCES "public"."teams"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "invitations" ADD CONSTRAINT "invitations_invited_by_users_id_fk" FOREIGN KEY ("invited_by") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "prices" ADD CONSTRAINT "prices_product_id_products_id_fk" FOREIGN KEY ("product_id") REFERENCES "public"."products"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "session" ADD CONSTRAINT "session_team_id_teams_id_fk" FOREIGN KEY ("team_id") REFERENCES "public"."teams"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "session" ADD CONSTRAINT "session_product_id_products_id_fk" FOREIGN KEY ("product_id") REFERENCES "public"."products"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "session" ADD CONSTRAINT "session_price_id_prices_id_fk" FOREIGN KEY ("price_id") REFERENCES "public"."prices"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "team_members" ADD CONSTRAINT "team_members_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "team_members" ADD CONSTRAINT "team_members_team_id_teams_id_fk" FOREIGN KEY ("team_id") REFERENCES "public"."teams"("id") ON DELETE no action ON UPDATE no action;
