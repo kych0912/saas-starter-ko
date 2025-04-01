d2a24e7494a7623a0e5195f3d034c7e546257593
YoungCheon Kim <kyt031522@gmail.com>
2025-04-02 03:06:56 +0900

    fix: user middleware 수정

--- middleware/user.ts
+++ middleware/user.ts
@@ -2,24 +2,19 @@ import { auth } from '@/auth';
 import { Session } from 'next-auth';
 import { NextResponse, NextRequest, NextFetchEvent } from 'next/server';
 import { CustomMiddleware } from './chain';
-const protectedRoutes = '/dashboard';
-
-type NextAuthRequest = NextRequest & { auth: Session | null };
-
-const authOptions = (request: NextAuthRequest): Response | void=>{
-  const { auth: session, nextUrl } = request;
-  const { pathname } = nextUrl;
-  const isProtectedRoute = pathname.startsWith(protectedRoutes);
-  const isAuthenticated = !!session;
-  
-  if (!isAuthenticated && isProtectedRoute) {
-    return NextResponse.redirect(new URL('/sign-in', request.url));
-  }    
-}
+const protectedRoutes = 'dashboard';
 
 export function withAuthMiddleware(middleware:CustomMiddleware){
   return async (request: NextRequest, event: NextFetchEvent, response: NextResponse) => {
-    auth(authOptions)
+    const session = await auth();
+
+    const { pathname } = request.nextUrl;
+    const isProtectedRoute = pathname.split('/').some(value => value === protectedRoutes);
+    const isAuthenticated = !!session;
+
+    if (!isAuthenticated && isProtectedRoute) {
+      return NextResponse.redirect(new URL('/sign-in', request.url));
+    }
     
     return middleware(request,event,response);
   }
