c03093c645239140f64d8538bba9e195ad01020e
YoungCheon Kim <kyt031522@gmail.com>
2025-04-26 19:17:53 +0900

    feat:pricing product steppay 연결 - 하드코딩 리팩토링

--- apps/client/app/[lng]/(dashboard)/pricing/PricingCard.tsx
+++ apps/client/app/[lng]/(dashboard)/pricing/PricingCard.tsx
@@ -2,34 +2,22 @@ import { Check } from 'lucide-react';
 import { SubmitButton } from './submit-button';
 import { createCheckoutAction } from '@/lib/payments/actions';
 import { useTranslation } from '@/app/i18n/useTranslation';
-import { DemoPeriodUnit, IntervalUnit } from '@/lib/payments/types/Product';
+import { DemoPeriodUnit, IntervalUnit, ProductPricedto, ProductProductDTO } from '@/lib/payments/types/Product';
 
 export default async function PricingCard({
-    name,
+    product,
     price,
-    interval,
-    features,
-    priceCode,
-    productCode,
     lng,
-    demoData,
-    unit
   }: {
-    name: string;
-    price: Record<string,number>;
-    interval: number;
-    features: string[];
-    priceCode?: string;
-    productCode?: string;
+    product: ProductProductDTO;
+    price: ProductPricedto;
     lng: string;
-    demoData: {
-      enabledDemo: boolean;
-      demoPeriod: number;
-      demoPeriodUnit: DemoPeriodUnit;
-    };
-    unit: IntervalUnit;
   }) {
     const {t} = await useTranslation(lng, 'pricing');
+    const { enabledDemo, demoPeriod, demoPeriodUnit } = product;
+    const { planName, currencyPrice, recurring, planDescription } = price;
+
+    const features = planDescription?.split('\n').map((feature) => feature.trim());
 
     function getUnit(unit: IntervalUnit | DemoPeriodUnit){
       switch(unit){
@@ -66,20 +54,20 @@ export default async function PricingCard({
     
     return (
       <div className="pt-6">
-        <h2 className="text-2xl font-medium text-foreground mb-2">{t(name)}</h2>
-        {demoData.enabledDemo && (
+        <h2 className="text-2xl font-medium text-foreground mb-2">{t(planName || '')}</h2>
+        {enabledDemo && (
           <p className="text-sm text-muted-foreground mb-4">
-            {t('trial', {trialDays: demoData.demoPeriod,unit:getUnit(demoData.demoPeriodUnit)})}
+            {t('trial', {trialDays: demoPeriod,unit:getUnit(demoPeriodUnit)})}
           </p>
         )}
         <p className="text-4xl font-medium text-foreground mb-6">
-          {getCurrency(Object.keys(price)[0])}{demicalFormatPrice(price[Object.keys(price)[0]])}{' '}
+          {getCurrency(Object.keys(currencyPrice)[0])}{demicalFormatPrice(currencyPrice[Object.keys(currencyPrice)[0]])}{' '}
           <span className="text-xl font-normal text-muted-foreground">
-            {t('price',{interval:interval,unit:getUnit(unit)})}
+            {t('price',{interval:recurring?.intervalCount || 1,unit:getUnit(recurring?.interval || IntervalUnit.MONTH)})}
           </span>
         </p>
         <ul className="space-y-4 mb-8">
-          {features.map((feature, index) => (
+          {features?.map((feature, index) => (
             <li key={index} className="flex items-start">
               <Check className="h-5 w-5 text-orange-500 mr-2 mt-0.5 flex-shrink-0" />
               <span className="text-foreground">{t(feature)}</span>
@@ -87,8 +75,8 @@ export default async function PricingCard({
           ))}
         </ul>
         <form action={createCheckoutAction}>
-          <input type="hidden" name="productCode" value={productCode} />
-          <input type="hidden" name="priceCode" value={priceCode} />
+          <input type="hidden" name="productCode" value={product.code} />
+          <input type="hidden" name="priceCode" value={price.code} />
           <SubmitButton lng={lng}/>
         </form>
       </div>
